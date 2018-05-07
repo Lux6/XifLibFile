c982bef758be31560122421081bc7eb593b7eb47
Abelatox <abel54225@gmail.com>
2018-05-07 08:45:35 +0200

    :D

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -13,47 +13,52 @@ import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class Controller implements Initializable{
+public class Controller implements Initializable {
 
-    @FXML private Button btnIn;
-    @FXML private Label lblRutaIn;
-    @FXML private TextField tfOut;
-    private FileChooser fileChooser;
-    private File fileIn;
-    private File fileOut;
+	@FXML
+	private Button btnIn;
+	@FXML
+	private Label lblRutaIn;
+	@FXML
+	private TextField tfOut;
+	private FileChooser fileChooser;
+	private File fileIn;
+	private File fileOut;
 
-    /**
-     * CERCA LA RUTA DEL FITXER D'ENTRADA
-     * @param ae
-     */
-    @FXML
-    public void onClickBtnIn(ActionEvent ae ) {
+	/**
+	 * CERCA LA RUTA DEL FITXER D'ENTRADA
+	 * 
+	 * @param ae
+	 */
+	@FXML
+	public void onClickBtnIn(ActionEvent ae) {
 
-        fileIn = fileChooser.showOpenDialog( null );
-        lblRutaIn.setText( fileIn.getPath() );
+		fileIn = fileChooser.showOpenDialog(null);
+		lblRutaIn.setText(fileIn.getPath());
 
-    }
+	}
 
-    /**
-     * XIFA/DESXIFRA EL DOCUMENT ESCOLLIT
-     * @param ae
-     */
-    @FXML
-    public void onClickBtnXif( ActionEvent ae ) {
+	/**
+	 * XIFA/DESXIFRA EL DOCUMENT ESCOLLIT
+	 * 
+	 * @param ae
+	 */
+	@FXML
+	public void onClickBtnXif(ActionEvent ae) {
 
-        fileOut = new File( tfOut.getText() );
-        XifLib xif = new XifLib();
-        xif.xifrar( fileIn, fileOut );
+		fileOut = new File(tfOut.getText());
+		XifLib xif = new XifLib();
+		xif.xifrar(fileIn, fileOut);
 
-        lblRutaIn.setText( "" );
-        tfOut.setText( "" );
+		lblRutaIn.setText("");
+		tfOut.setText("");
 
-    }
+	}
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
 
-        fileChooser = new FileChooser();
+		fileChooser = new FileChooser();
 
-    }
+	}
 }
